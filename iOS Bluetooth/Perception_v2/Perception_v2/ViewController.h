f3934e033f4ab7c73cf08a76cf18725ed3b503de
Rishi Ved <rnved@andrew.cmu.edu>
2016-03-30 18:33:00 -0400

    UI and button function call complete

---
+++ iOS Bluetooth/Perception_v2/Perception_v2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Perception_v2
+//
+//  Created by Rishi Ved on 3/30/16.
+//  Copyright © 2016 Rishi Ved. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS Bluetooth/Perception_v2/Perception_v2/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Perception_v2
+//
+//  Created by Rishi Ved on 3/30/16.
+//  Copyright © 2016 Rishi Ved. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+    // MARK: Actions
+- (IBAction)ConnectToBLEModule:(UIButton *)sender {
+    // TODO: Insert code to connect BLE Module and advertise custom UUID.
+    printf("Hello World\n");
+}
+
+@end
